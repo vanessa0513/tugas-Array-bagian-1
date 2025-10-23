2daf25363b6fa3b5865cbc61d736a8b52742f070
VanesssaE.K <vanessa_kapugu_34rpl@student.smktelkom-mlg.sch.id>
2025-10-23 09:55:47 +0700

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,10 @@
+public class Array1 {
+    public static void main(String[] args) {
+        String[] namaKota = {"Malang", "Surabaya", "Bandung", "Jakarta", "Semarang", "Yogyakarta", "Bali", "Medan", "Padang", "Makassar"};
+        
+        System.out.println("Daftar Nama kota :");
+        for (int i = 0; i < namaKota.length; i++) {
+            System.out.println(namaKota[i]);
+        }
+    }
+}
